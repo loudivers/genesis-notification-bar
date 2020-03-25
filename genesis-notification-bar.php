8a76e63292d6a75cf9fc42b22d297adaad7006e8
Lou Divers <lou.divers@gmail.com>
2020-03-24 21:09:47 -0400

    First Commit
    Initial really simple build to add widget are and display it above the site-header of the Genesis Framework.

---
+++ genesis-notification-bar.php
@@ -0,0 +1,37 @@
+<?php
+/*
+   Plugin Name: Genesis Notification Bar
+   Plugin URI: http://wordpress.org/extend/plugins/genesis-notification-bar/
+   Version: 0.1
+   Author: Lou Divers
+   Description: Notification bar for Genesis framework
+   Text Domain: genesis-notification-bar
+   License: GPLv3
+  */
+
+// If this file is called directly, abort.
+defined( 'ABSPATH' ) or die( 'Go Catch A Case Of Corona You Dirty Hacker' );
+
+// Register notification bar widget area.
+genesis_register_sidebar(
+	array(
+		'id'          => 'notification-bar',
+		'name'        => __( 'Notification Bar', 'genesis' ),
+		'description' => __( 'Widgets in this widget area will display above everything in the header.', 'genesis' ),
+	)
+);
+
+// Priority One Places this above everything 
+add_action( 'genesis_before_header', 'display_notification', 1 );
+/**
+ * Add top widget.
+ */
+function display_notification() {
+	genesis_widget_area(
+		'notification-bar',
+		array(
+			'before' => '<div class="notification-bar"><div class="widget-area">',
+			'after'  => '</div></div>',
+		)
+	);
+}
